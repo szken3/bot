119c1b5be9b0f204f9433326f527cbe423123724
Ken Suzuki <dddlia3@gmail.com>
2020-11-09 18:30:54 +0900

    apply composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cd98ebb2f692b3002909387d31b474148dd945bc',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cd98ebb2f692b3002909387d31b474148dd945bc',
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '6.0.0',
+      'version' => '6.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '88fc4afb3c50315708b1c4487845d9c03c44a62b',
+    ),
+  ),
+);
