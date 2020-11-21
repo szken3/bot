712c0ae3977647e975ed7ba761705f97838696e9
Ken Suzuki <dddlia3@gmail.com>
2020-11-21 10:46:01 +0900

    edit index.php send message

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 
+// Composer でライブラリの一括読み込み
 require_once __DIR__ . '/vender/autoload.php';
 
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANEL_ACCESS_TOKEN'));
+
+$bot = new \LINE\LINEBot\($httpClient, ['chanelSelect' => getenv('CHANEL_SECRET')]);
+
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+foreach($events as $event){
+  $bot->replyText($event->getReplyToken(), 'TextMessage');
+}
+
  ?>
