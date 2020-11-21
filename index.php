303722fdaa191631d9cc22db66c84983d44c7c2d
Ken Suzuki <dddlia3@gmail.com>
2020-11-21 10:56:03 +0900

    fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Composer でライブラリの一括読み込み
-require_once __DIR__ . '/vender/autoload.php';
+require_once __DIR__ . './vender/autoload.php';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANEL_ACCESS_TOKEN'));
 
