4dc8aa06929b6aba56294089df407064d6283846
Ken Suzuki <dddlia3@gmail.com>
2020-11-28 10:21:24 +0900

    no message

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 
 // Composer でライブラリの一括読み込み
-//require_once __DIR__ . './vender/autoload.php';
 require_once __DIR__ . '/vendor/autoload.php';
-
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANEL_ACCESS_TOKEN'));
 
-$bot = new \LINE\LINEBot($httpClient, ['chanelSelect' => getenv('CHANEL_SECRET')]);
+$bot = new \LINE\LINEBot($httpClient, ['chanelSelect' => getenv('CHANNEL_SECRET')]);
 
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
